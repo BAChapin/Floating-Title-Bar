51c6c6c3622a8f67fce5cc1e6f524b16a458ed48
Brett Chapin <brett.chapin89@gmail.com>
2021-08-11 12:16:04 -0400

    Initial Commit

---
+++ Shared/FBBView_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBBView_TestApp.swift
+//  Shared
+//
+//  Created by Brett Chapin on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FBBView_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
